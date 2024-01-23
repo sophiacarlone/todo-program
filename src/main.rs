a3d941c3f7eb0abc5fe5d6d7091e265abb635288
sophiacarlone <sophiamcar@gmail.com>
2024-01-23 17:59:22 -0500

    not liking this so far

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,42 @@
 // Sophia Carlone
 //Todo program
 
-use std::io;
+//TODO: tasks need an bool (done) and optional due date (and maybe priority)
+
+use std::io::{self, Read};
 fn main() {
     println!("heyyyy we are starting! A gaboghoul");
 
     // let mut  = Vec::new();
     let mut tasks = Vec::new();
-
     tasks.push("hi");
 
-    println!("please enter your first task");
-    let mut temp = String::new();
+    let mut option = String::new();
+    println!("(1) Insert, (2) Delete, (3) Move");
     io::stdin()
-        .read_line(&mut temp).expect("input failed"); //for the .expect, there is a Ok or an Err returned. If and Err is returned, it prints out the message after a panic
-    println!("{} was inputed", temp); //how to print out
-    tasks.push(&mut temp);
+        .read_line(&mut option).expect("input failed");
+
+    if option == "1"{
+        let mut temp = String::new();
+        println!("please enter your first task");
+        io::stdin()
+            .read_line(&mut temp).expect("input failed"); //for the .expect, there is a Ok or an Err returned. If and Err is returned, it prints out the message after a panic
+        println!("{} was inputed", temp); //how to print out
+        tasks.push(&mut temp);    
+    }
+    else if option == "2" {
+
+    }
+    else if option == "3" {
+        
+    }
+    else{
+        println!("you've done something wrong");
+    }
 
     println!("separation");
     for x in &tasks {
         println!("{x}");
     }
+
 }
