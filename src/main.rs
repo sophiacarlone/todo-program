2bb7ad2aaa76cdf3c489460df238a665827dd8f7
sophiacarlone <sophiamcar@gmail.com>
2024-02-03 23:17:46 -0500

    yahoo

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,74 @@
 //Sophia Carlone
 
-use druid::{AppLauncher, Widget, WindowDesc, Color};
-use druid::widget::{Container, Flex, Split, Label, List};
-use im::Vector; //cannot use standard vector
+use druid::{AppLauncher, Color, Data, Lens, Widget, WidgetExt, WindowDesc, Env};
+use druid::widget::{Button, Checkbox, Container, Flex, Label, LensWrap, List, Split};
+use im::Vector;
+use task_data_derived_lenses::label; //cannot use standard vector
+
 
 /******DATA*****/
-struct Task_Data {
+#[derive(Clone, Data, Lens, Default)]
+struct TaskData {
     label: String,
-    priority: i32,    
+    priority: i32,   
+    completed: bool, 
 }
-type TodoList = Vector<Task_Data>;
+
+#[derive(Clone, Data, Lens, Default)]
+struct TodoList{
+    todos: Vector<TaskData>,
+}
 
 
 /*****FUNCTIONS*****/
-fn build_ui() -> impl Widget<i32>{ //root widget
-    Split::columns(
-        Container::new(
-        Flex::column()
-            .with_flex_child(Label::new("first item"), 1.0)
-            .with_flex_child(Label::new("second item"), 1.0)
-            .with_flex_child(Label::new("third item"), 1.0)
-            .with_flex_child(Label::new("fourth item"), 1.0),
-    )
-    .border(Color::grey(0.6), 2.0),
-    Container::new(
-        Flex::column()
-            .with_flex_child(Label::new("Button placeholder"), 1.0)
-            .with_flex_child(Label::new("Textbox placeholder"), 1.0),
-    )
-    .border(Color::grey(0.6), 2.0),)
-    .draggable(true)
+fn build_ui() -> impl Widget<TodoList>{ //root widget
+    let day = List::new( || {
+        Flex::row()
+            .with_child(Checkbox::new("").lens(TaskData::completed))
+            .with_child(Label::new(|data: &TaskData, _: &Env| data.label.clone()))
+    }).lens(TodoList::todos);
+
+    Flex::column()
+        .with_child(day)
+        .with_child(Button::new("add task")
+            .on_click(|_, data: &mut TodoList, _|{
+                data.todos.push_back(TaskData {label: String::from("hi"), priority: 1, completed: false});
+            }))
+    // Split::columns(
+    //     Container::new(
+    //         LensWrap::new(
+    //             List::new(|| Label::dynamic(|data, _| format!("List item: {data}"))),
+    //             TodoList::todos::label,
+    //         )
+
+    //     )
+    // .border(Color::grey(0.6), 2.0),
+    // Container::new(
+    //     Flex::column()
+    //     .with_flex_child(Button::new("add item"), 1.0)
+    //     .with_flex_child(Label::new("Textbox placeholder"), 1.0),
+    // )
+    // .border(Color::grey(0.6), 2.0),)
+    // .draggable(true)
 }
 
 fn main() {
     let main_window = WindowDesc::new(build_ui())
         .window_size((600.0, 400.0))
         .title("My ToDos");
-    let x = 5;
-    let initial_data = x;
+
+    // let mut test = TaskData{
+    //     label: String::from("jo"),
+    //     priority: 1,
+    //     completed: false,
+    // };
+    // let testvec = Vector<TaskData>;
+
+    // let mut initial_data = TodoList{
+    //     todos: 
+    // };
 
     AppLauncher::with_window(main_window)
-        .launch(initial_data)
+        .launch(TodoList::default())
         .expect("Failed to launch application");
 }
